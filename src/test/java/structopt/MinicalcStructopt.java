8ff8138ff1839243c1143b52ac5e4d2b7b5900a7
Petr Kozelka <pkozelka@gmail.com>
2019-04-26 10:43:36 +0200

    design drastically simplified

--- src/test/java/structopt/MinicalcStructopt.java
+++ src/test/java/structopt/MinicalcStructopt.java
@@ -4,21 +4,9 @@ import java.util.Arrays;
 
 public class MinicalcStructopt {
 
-    interface MinicalcCli {
-
+    interface GlobalParams {
         @Structopt(defaultValue = "%d", shortOpt = 'f')
         String format();
-
-        CalcOperation subcommand();
-    }
-
-    enum CalcOperation {
-        @StructoptCommand(commandParams = PlusParams.class)
-        PLUS,
-        @StructoptCommand(commandParams = MinusParams.class)
-        MINUS,
-        @StructoptCommand(name = "log", commandParams = LogParams.class)
-        LOGARITHM
     }
 
     interface PlusParams {
@@ -27,43 +15,43 @@ public class MinicalcStructopt {
 
     interface MinusParams {
         int op1();
-
         int op2();
     }
 
     interface LogParams {
         @Structopt(defaultValue = "10", shortOpt = 'b', longOpt = "base")
         int base();
-
         int number();
     }
 
-    public static void main(String[] args) throws Exception {
-        final StructoptParser<MinicalcCli> cliParser = new StructoptParser<>(MinicalcCli.class);
-        final MinicalcCli cli = cliParser.parse(args);
-        final Object params = cliParser.subcommandParams(cli.subcommand());
-        switch (cli.subcommand()) {
-            case PLUS:
-                {
-                    final PlusParams pp = (PlusParams) params;
-                    final int result = Arrays.stream(pp.operands()).sum();
-                    System.out.printf(cli.format(), result);
-                }
-                break;
-            case MINUS:
-                {
-                    final MinusParams mp = (MinusParams) params;
-                    final int result = mp.op1() - mp.op2();
-                    System.out.printf(cli.format(), result);
-                }
-                break;
-            case LOGARITHM:
-                {
-                    final LogParams p = (LogParams) params;
-                    final double result = Math.log(p.number() / Math.log(p.base()));
-                    System.out.printf(cli.format(), result);
-                }
-                break;
+    //TODO this will be class, with some special support
+    static interface StandardCommandsImplDraft {
+        void help(String[] args);
+        void version();
+        void completions(String shell);
+        void invalidCommand(String command);
+
+        void paramlessExecution();
+    }
+
+    static class MinicalcCliImpl {
+        void plus(GlobalParams cli, PlusParams pp) {
+            final int result = Arrays.stream(pp.operands()).sum();
+            System.out.printf(cli.format(), result);
         }
+
+        void minus(GlobalParams cli, MinusParams mp) {
+            final int result = mp.op1() - mp.op2();
+            System.out.printf(cli.format(), result);
+        }
+
+        void log(GlobalParams cli, LogParams p) {
+            final double result = Math.log(p.number() / Math.log(p.base()));
+            System.out.printf(cli.format(), result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        StructoptParser.execute(args, MinicalcCliImpl.class,StandardCommandsImplDraft.class);
     }
 }
--- src/test/java/structopt/StructoptParser.java
+++ src/test/java/structopt/StructoptParser.java
@@ -1,19 +1,7 @@
 package structopt;
 
-import java.util.Optional;
-
-public class StructoptParser<C> {
-    private final Class<C> definition;
-
-    public StructoptParser(Class<C> definition) {
-        this.definition = definition;
-    }
-
-    public C parse(String... args) throws Exception {
-        throw new UnsupportedOperationException();
-    }
-
-    public Object subcommandParams(Enum subcommandConstant) {
+public class StructoptParser {
+    public static void execute(String[] args, Class<?>... implClasses) {
         throw new UnsupportedOperationException();
     }
 }
